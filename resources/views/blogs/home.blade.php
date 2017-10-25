074da474d22b113649ede0154807e277da83fb1f
Yudi Rahman <yudiwisudawan@gmail.com>
2017-10-25 11:21:28 +0700

    Blog Ku Ka Indonesia. (seeder, composer update, db migrate)

---
+++ resources/views/blogs/home.blade.php
@@ -0,0 +1,122 @@
+@extends('app')
+
+@section('content')
+    <div class="uk-margin-top uk-margin-bottom">
+        {{ Html::image('http://iscaprobus.co.uk/wp-content/uploads/2016/05/book.jpg','alt_image', array( 'width' => 1200, 'height' => 300 )) }}
+    </div>
+
+    <div class="uk-grid-small uk-grid-match uk-child-width-expand@s uk-text-justify" uk-grid>
+        <div>
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li class="uk-text-lead"><a href="#">Content</a></li>
+                <li class="uk-nav-divider"></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li class="uk-text-lead"><a href="#">Features</a></li>
+                <li class="uk-nav-divider"></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li class="uk-text-lead"><a href="#">Discover</a></li>
+                <li class="uk-nav-divider"></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="uk-nav uk-navbar-dropdown-nav">
+                <li class="uk-text-lead"><a href="#">Tags</a></li>
+                <li class="uk-nav-divider"></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+                <li><a href="#">Item</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <h2 class="uk-heading-line uk-text-center"><span>Top Stories</span></h2>
+
+    <div class="uk-grid uk-grid-small uk-child-width-1-3@m uk-child-width-1-2@s">
+        <div class="uk-text-center uk-margin-bottom">
+            <a href="">
+                <div class="uk-inline-clip uk-transition-toggle">
+                    <img class="uk-transition-scale-up uk-transition-opaque" src="https://www.w3schools.com/howto/img_avatar.png" alt="">
+                    <div class="uk-transition-fade uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle">
+                        <li class="uk-text-large uk-text-muted uk-text-uppercase">Quis nulla et eiusmod aut distinctio Illum pariatur Nemo deleniti officiis ipsam cupidatat</li>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="uk-text-center uk-margin-bottom">
+            <a href="">
+                <div class="uk-inline-clip uk-transition-toggle">
+                    <img class="uk-transition-scale-up uk-transition-opaque" src="https://www.w3schools.com/howto/img_avatar.png" alt="">
+                    <div class="uk-transition-fade uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle">
+                        <li class="uk-text-large uk-text-muted uk-text-uppercase">Quis nulla et eiusmod aut distinctio Illum pariatur Nemo deleniti officiis ipsam cupidatat</li>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="uk-text-center uk-margin-bottom">
+            <a href="">
+                <div class="uk-inline-clip uk-transition-toggle">
+                    <img class="uk-transition-scale-up uk-transition-opaque" src="https://www.w3schools.com/howto/img_avatar.png" alt="">
+                    <div class="uk-transition-fade uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle">
+                        <li class="uk-text-large uk-text-muted uk-text-uppercase">Quis nulla et eiusmod aut distinctio Illum pariatur Nemo deleniti officiis ipsam cupidatat</li>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="uk-text-center uk-margin-bottom">
+            <a href="">
+                <div class="uk-inline-clip uk-transition-toggle">
+                    <img class="uk-transition-scale-up uk-transition-opaque" src="https://www.w3schools.com/howto/img_avatar.png" alt="">
+                    <div class="uk-transition-fade uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle">
+                        <li class="uk-text-large uk-text-muted uk-text-uppercase">Quis nulla et eiusmod aut distinctio Illum pariatur Nemo deleniti officiis ipsam cupidatat</li>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="uk-text-center uk-margin-bottom">
+            <a href="">
+                <div class="uk-inline-clip uk-transition-toggle">
+                    <img class="uk-transition-scale-up uk-transition-opaque" src="https://www.w3schools.com/howto/img_avatar.png" alt="">
+                    <div class="uk-transition-fade uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle">
+                        <li class="uk-text-large uk-text-muted uk-text-uppercase">Quis nulla et eiusmod aut distinctio Illum pariatur Nemo deleniti officiis ipsam cupidatat</li>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="uk-text-center uk-margin-bottom">
+            <a href="">
+                <div class="uk-inline-clip uk-transition-toggle">
+                    <img class="uk-transition-scale-up uk-transition-opaque" src="https://www.w3schools.com/howto/img_avatar.png" alt="">
+                    <div class="uk-transition-fade uk-position-cover uk-overlay uk-overlay-default uk-flex uk-flex-center uk-flex-middle">
+                        <li class="uk-text-large uk-text-muted uk-text-uppercase">Quis nulla et eiusmod aut distinctio Illum pariatur Nemo deleniti officiis ipsam cupidatat</li>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+
+
+
+
+    </div>
+
+@endsection
+
