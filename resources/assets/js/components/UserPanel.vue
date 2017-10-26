f096a414ccd5d7b3807312651e3046c72ac62baa
DESKTOP-H3T7863\KuKaJack <joko.aolia98@gmail.com>
2017-10-26 15:30:29 +0700

    drop bag

--- resources/assets/js/components/UserPanel.vue
+++ resources/assets/js/components/UserPanel.vue
@@ -15,20 +15,33 @@
 
     <li>
       <a class="uk-button uk-button-text uk-button-small" :href="bagLink"> <b>B A G</b></a>
-      <div uk-drop="pos: bottom-right; delay-hide:0" v-if="bags.length != 0">
-            <div class="uk-card uk-card-body uk-card-small uk-card-default">
+      <div class="uk-card-border uk-background-default uk-card" uk-drop="pos: bottom-right; delay-hide:0" v-if="bags.length != 0">
+            <div class="uk-card-body uk-card-small ">
               <div class="uk-grid-small" uk-grid v-for="bag in bags">
                 <div class="uk-width-1-3">
                   <img :src="'/'+bag.options.photo" :alt="bag.name">
                 </div>
                 <div class="uk-width-2-3">
                   <div class="uk-panel">
-                    <h4 class="uk-margin-remove uk-padding-remove">{{ bag.name }}</h4>
-                    <h5 class="uk-margin-remove uk-padding-remove">{{ bag.options.currency }} {{ bag.price }}</h5>
+                    <span class="uk-text-small"><b>{{ bag.name }}</b></span><br>
+                    <span class="uk-text-small">{{ bag.options.currency }} {{ bag.price }} </span><br>
                     <span class="uk-text-meta">color : {{ bag.options.color }}</span><br>
                     <span class="uk-text-meta">size  : {{ bag.options.size }}</span><br>
+                    <button type="button" class="uk-button uk-button-text uk-button-small" name="button"><span class="uk-icon" uk-icon="icon: pencil; ratio: 0.8"></span>edit</button>
+                    <button type="button" class="uk-button uk-button-text uk-button-small" name="button"><span class="uk-icon" uk-icon="icon: trash; ratio: 0.8"></span>remove</button>
                   </div>
                 </div>
+              </div>
+            </div>
+            <div class="uk-card-footer uk-padding-remove uk-background-muted">
+              <div class="uk-text-center">
+                <a href="#">see all your shop</a>
+              </div>
+            </div>
+            <div class="uk-card-footer uk-padding-small">
+              <div class="uk-text-center">
+                <b>SUB TOTAL: IDR 1200000</b>
+                <input type="submit" class="uk-button-secondary uk-button uk-button-small uk-width-1-1" name="" value="CHECKOUT NOW">
               </div>
             </div>
         </div>
