8594f99789f31b0effecfec19f1e8c8bf0686235
Samsul Anwar <samsul@rebelworks.co>
2017-11-09 11:24:29 +0700

    quick shop most populer

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -46,6 +46,7 @@ class PageController extends BaseApiController
             
             $popular = $popular->map(function ($entry) {
                 return [
+                    'id' => $entry->id,
                     'name' => $entry->name,
                     'slug' => $entry->slug,
                     'price' => $entry->sell_price,
@@ -76,6 +77,24 @@ class PageController extends BaseApiController
             })->toArray();
 
             return $this->success($related, 200, true);
+        } catch (Exception $e) {
+            return $this->error($e, 400, true);
+        }
+    }
+
+    public function product($id = null)
+    {
+        try {
+            $product = (new ProductRepository)
+                ->getProductById($id);
+
+            $product->images->toArray();
+
+            $product->stocks->toArray();
+
+            $product = collect($product)->all();
+
+            return $this->success($product, 200, true);
         } catch (Exception $e) {
             return $this->error($e, 400, true);
         }
