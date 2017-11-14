81dc9ef653d6eb8f6fe3711a5ecc8622192cc26d
DESKTOP-H3T7863\KuKaJack <joko.aolia98@gmail.com>
2017-11-14 16:19:05 +0700

    push bag ndisek, check out garap nko

--- resources/views/pages/bag.blade.php
+++ resources/views/pages/bag.blade.php
@@ -5,20 +5,13 @@
   <div class="uk-grid-small uk-margin-top">
       @include('partials.alert')
   </div>
-  <div class="uk-grid-small uk-margin-top">
-    <h2 class="uk-text-center">SHOPPING BAG</h2>
-    <p class="uk-text-center">NEED HELP?  CALL US: +44 (0)20 3471 4090 |  <a href="#" class="uk-text-muted">EMAIL CUSTOMER CARE</a> |
-      <a href="#" class="uk-text-muted">SHIPPING INFORMATION</a>  |  <a href="#" class="uk-text-muted">RETURNS & EXCHANGES</a></p>
-    <hr>
-    <p class="uk-text-right"><button class="uk-button uk-button-secondary uk-text-bold uk-padding-small-right">PROCEED TO PURCHASE</button></p>
-  </div>
   <bag
     bag_link="{{ route('persist.bag') }}"
     wishlist_link="{{ route('persist.wishlist') }}"
     auth="{{ Auth::check() ? 1 : 0 }}"
   ></bag>
 
-  <p class="uk-text-right"><button class="uk-button uk-button-secondary uk-text-bold uk-padding-small-right">PROCEED TO PURCHASE</button></p>
+  
   <hr>
   <div class="uk-grid-small uk-margin-small-bottom uk-margin-top">
     <div class="uk-panel uk-text-center">
