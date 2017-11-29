39ef1d612e027b92ad9e1ecbab613ba524ed9557
DESKTOP-H3T7863\KuKaJack <joko.aolia98@gmail.com>
2017-11-29 17:25:33 +0700

    order design

--- resources/views/pages/checkout/checkout_finish.blade.php
+++ resources/views/pages/checkout/checkout_finish.blade.php
@@ -1,30 +1,40 @@
 @extends('app_checkout')
 @section('content')
 <div class="uk-container uk-container-small">
-    <div class="uk-grid-small uk-margin-top" uk-grid>
+    <div class="uk-grid-small uk-margin-top uk-flex uk-flex-center" uk-grid>
         <div class="uk-width-3-4@m">
-          <table class="uk-table">
-            <tr>
-              <td>No Order</td><td colspan="3">{{ $order->order_code }}</td>
-            </tr>
-            <tr>
-              <td>Product Name</td><td>Product Price</td><td>Qty</td><td>Subtotal</td>
-            </tr>
-            @foreach($detail as $product)
-              @php 
-                $subtotal = $product['price'] * $product['qty'];
-              @endphp
-              <tr>
-                <td>{{ $product['product_name'] }}</td><td>{{ $product['price'] }}</td><td>{{ $product['qty'] }}</td><td>{{ $subtotal }}</td>
-              </tr>
-            @endforeach
-            <tr>
-              <td colspan="3">Shipping Cost</td><td>{{ $shipping }}</td>
-            </tr>
-            <tr>
-              <td colspan="3">Total</td><td>{{ $total +  $shipping}}</td>
-            </tr>
-          </table>
+          <h3>Your Checkout Process is almost done!</h3>
+          <div class="uk-card uk-card-default uk-card-border">
+            <div class="uk-card-body">
+              Order Number : <br> <span class="uk-label uk-text-lead uk-light">{{ $order->order_code }}</span> 
+              <table class="uk-table uk-table-divider uk-table-hover">
+                <thead>
+                  <tr>
+                    <th>Product Name</th><th>Product Price</th><th>Qty</th><th>Subtotal</th>
+                  </tr>
+                </thead>
+                  <tbody>
+                  @foreach($detail as $product)
+                    @php
+                      $subtotal = $product['price'] * $product['qty'];
+                    @endphp
+                    <tr>
+                      <td>{{ $product['product_name'] }}</td><td>{{ $product['price'] }}</td><td>{{ $product['qty'] }}</td><td>{{ $subtotal }}</td>
+                    </tr>
+                  @endforeach
+                  <tr>
+                    <td colspan="3">Shipping Cost</td><td>{{ $shipping }}</td>
+                  </tr>
+                  <tr>
+                    <td colspan="3"><h4><b>Total</b></h4></td><td><h4><b>{{ $total +  $shipping}}</b></h4></td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="uk-text-center">
+                <input type="submit" name="pay" value="pay now" class="uk-button uk-button-danger uk-width-1-2 uk-text-center">
+              </div>
+            </div>
+          </div>
         </div>
     </div>
 </div>
