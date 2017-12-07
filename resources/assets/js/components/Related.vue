8443b2591ddaac5d435cdb7b31f9eb06662926b6
Samsul Anwar <samsul@rebelworks.co>
2017-12-07 11:27:45 +0700

    unit stock on view product

--- resources/assets/js/components/Related.vue
+++ resources/assets/js/components/Related.vue
@@ -106,8 +106,8 @@
                   <!-- <div uk-form-custom="target: > * > span:first"> -->
                       <select name="size" v-model="size" v-validate="'required'" class="uk-select">
                           <option :value="null" disabled>Choose Size</option>
-                          <option v-for="stock in stocks" :value="stock.sku">
-                            {{ stock.size }}
+                          <option v-for="stock in stocks" :value="stock.sku" :disabled="stock.unit <= 0">
+                            {{ stock.size }} {{ stock.unit | unit }}
                           </option>
                       </select>
                       <!-- <button class="uk-button uk-button-default" type="button" tabindex="-1">
@@ -320,6 +320,18 @@
               });
           }
         });
+      }
+    },
+
+    filters: {
+      unit: function (unit) {
+          if (unit < 10) {
+              if (unit <= 0) {
+                  return '(Sold Out)';
+              } else {
+                  return '(Only '+unit+' Left)';
+              }
+          }
       }
     }
   }
