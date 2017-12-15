af82415d71d9a1d952f1f01efda23ae227c3c335
Samsul Anwar <samsul@rebelworks.co>
2017-12-15 12:31:33 +0700

    build for aws

---
+++ public/build/js/custom-81ec7bfa55.js
@@ -0,0 +1,1 @@
+function getTokenValue(){return document.querySelector("#csrf-token").getAttribute("content")}$(window).on("load",function(){$("#status").fadeOut(),$("#preloader").delay(350).fadeOut("slow"),$("body").delay(350).css({overflow:"visible"})});
