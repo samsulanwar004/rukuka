9ed6cc0867dea291043084e72b4e4d26896fe52e
Samsul Anwar <samsul@rebelworks.co>
2017-12-15 12:41:29 +0700

    delete assets

--- public/js/custom.js
+++
@@ -1,1 +0,0 @@
-function getTokenValue(){return document.querySelector("#csrf-token").getAttribute("content")}$(window).on("load",function(){$("#status").fadeOut(),$("#preloader").delay(350).fadeOut("slow"),$("body").delay(350).css({overflow:"visible"})});
