e5c8c2244293b05183234a5c11117ff81ad77be6
Samsul Anwar <samsul@rebelworks.co>
2018-01-19 15:27:28 +0700

    test 2 designer

--- resources/views/pages/designer.blade.php
+++ resources/views/pages/designer.blade.php
@@ -12,41 +12,48 @@
             @php
                 $previous = null;
                 $isPrinted = false;
-                $listAlphabeth = null;
             @endphp
 
-            @foreach($designers as $value)
-                @php
-                $listAlphabeth[] = strtolower(substr($value['slug'], 0, 1));
-                $listAlphabeth = array_unique($listAlphabeth)
-                @endphp
-            @endforeach
-
-            @foreach($listAlphabeth as $listAlphabethValue)
+            @foreach($alpabeths as $abjad)
                 @foreach($designers as $value)
-                    @php $firstLetter = strtolower(substr($value['slug'], 0, 1)); @endphp
-                    @if($firstLetter == $listAlphabethValue)
-                        @php $isPrinted = true; break; @endphp
+                    @php 
+                        $firstLetter = strtolower(substr($value['slug'], 0, 1)); 
+                    @endphp
+                    @if($firstLetter == $abjad)
+                        @php 
+                            $isPrinted = true; 
+                            break; 
+                        @endphp
                     @endif
                 @endforeach
 
                 @if($isPrinted)
-                    <h3 class="uk-heading-line"><span>{{strtoupper($listAlphabethValue)}}</span> </h3>
+                    <h3 class="uk-heading-line">
+                        <span>
+                            {{strtoupper($abjad)}}
+                        </span> 
+                    </h3>
                     <div class="uk-column-1-3@m uk-column-1-2@s">
                     @foreach($designers as $value)
-                        @php $firstLetter = strtolower(substr($value['slug'], 0, 1)); @endphp
-                        @if($firstLetter == $listAlphabethValue)
+                        @php 
+                            $firstLetter = strtolower(substr($value['slug'], 0, 1)); 
+                        @endphp
+                        @if($firstLetter == $abjad)
                         <div>
-                            <a href="/shop/designers/{{$value['slug']}}">{{ucfirst(strtolower($value['name']))}}</a>
+                            <a href="/shop/designers/{{$value['slug']}}">
+                                {{ucfirst(strtolower($value['name']))}}
+                            </a>
                             <div uk-drop="pos: right-center">
                                 <div class="uk-card uk-card-default uk-card-small">
                                     <div class="uk-card-header">
                                         <div class="uk-grid-small uk-flex-middle" uk-grid>
                                             <div class="uk-width-auto">
-                                                    <img class="uk-border-circle" width="80" height="80" src="{{uploadCDN($value['photo'])}}" onerror="this.src = '{{imageCDN(config('common.default.image_6'))}}'">
+                                                <img class="uk-border-circle" width="80" height="80" src="{{uploadCDN($value['photo'])}}" onerror="this.src = '{{imageCDN(config('common.default.image_6'))}}'">
                                             </div>
                                             <div class="uk-width-expand">
-                                                <h3 class="uk-card-title uk-margin-remove-bottom">{{$value['name']}}</h3>
+                                                <h3 class="uk-card-title uk-margin-remove-bottom">
+                                                    {{$value['name']}}
+                                                </h3>
                                             </div>
                                         </div>
                                     </div>
@@ -60,8 +67,9 @@
                     @endforeach
                     </div>
                 @endif
-
-                @php $isPrinted = false; @endphp
+                @php 
+                    $isPrinted = false; 
+                @endphp
             @endforeach
 
         </div>
