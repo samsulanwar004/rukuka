ea6150f18c1fefbdc4cf015792432d4273eee311
Samsul Anwar <samsul@rebelworks.co>
2018-01-24 16:31:59 +0700

    404 change

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,24 +1,45 @@
-@extends('app')
+<!DOCTYPE html>
+<html>
+<head> <title>404</title>
+<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet" type="text/css">
 
-@section('content')
-    {{-- <div class="uk-container uk-container-small">
-        <div class="uk-grid-small uk-margin-top uk-margin-bottom">
-            <div>
-                <div class="uk-card uk-card-default uk-card-large uk-card-body">
-                    <h3 class="uk-card-title">
-                        THIS PAGE CANNOT BE FOUND (404)
-                    </h3>
-                    WE APOLOGISE
-                    It seems we cannot find the page you are looking for. It has either been moved, deleted or no longer exists.
+<style>
+    html, body {
+       height: 100%;
+    }
 
-                    <div class="uk-grid-small uk-margin-xlarge-top uk-margin-small-bottom">
-                        <div class="uk-panel uk-text-center">
-                            <a href="{{ route('index') }}"><button class="uk-button uk-button-secondary">Back To Home</button></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    body {
+       margin: 0;
+       padding: 0;
+       width: 100%;
+       color: #666666;
+       display: table;
+       font-weight: 300;
+       font-family: 'Lato';
+    }
+
+    .container {
+       text-align: center;
+       display: table-cell;
+       vertical-align: middle;
+    }
+
+    .content {
+       text-align: center;
+       display: inline-block;
+    }
+
+    .title {
+       font-size: 100px;
+       margin-bottom: 40px;
+    }
+</style>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <div class="title">Opss...Page Not Found!</div>
         </div>
-
-    </div> --}}
-@endsection
+    </div>
+</body>
+</html>
