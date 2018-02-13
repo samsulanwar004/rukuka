46e99154a0a5f0d379ccda45089f363d1ac650cc
AoliaJack <joko.aolia98@gmail.com>
2018-02-13 10:35:21 +0700

    footer added, lang, font changed, bug style

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -132,9 +132,9 @@
             {{-- <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul> --}}
 
         </div>
-        <hr class="uk-margin-large-top" style="border-color: #333; border-width: 3px">
+        <hr class="uk-margin" style="border-color: #333; border-width: 3px">
         <h2 class="uk-text-center uk-visible@m">Happenings Right Now!</h2>
-        <h4 class="uk-text-center uk-hidden@m">Happenings Right Now!</h4>
+        <h4 class="uk-text-center uk-margin-small uk-hidden@m">Happenings Right Now!</h4>
 
         <div uk-slider="clsActivated: uk-transition-active;">
                 <div class="uk-position-relative uk-visible-toggle">
