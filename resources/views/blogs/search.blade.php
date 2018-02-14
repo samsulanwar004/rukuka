72f69dfc48f68d9ca066f0b05914ba5dc6387e73
Yudi Rahman <yudiwisudawan@gmail.com>
2018-02-14 11:18:25 +0700

    Search and Color Mobile, and add default image 7

--- resources/views/blogs/search.blade.php
+++ resources/views/blogs/search.blade.php
@@ -26,7 +26,7 @@
                         <div class="uk-inline-clip uk-transition-toggle uk-light">
                             <a href="{{ URL::to('blog/'.$post->slug)}}" class="uk-link-reset">
                                 <div style="background: rgba(0,0,0,.2);" class="uk-position-cover"></div>
-                                <img src="{{ uploadCDN($post->photo_1) }}" alt="{{$post->title}}" onerror="this.src = '{{imageCDN(config('common.default.image_2'))}}'">
+                                <img src="{{ uploadCDN($post->photo_1) }}" alt="{{$post->title}}" onerror="this.src = '{{imageCDN(config('common.default.image_7'))}}'">
                                 <div class="uk-card uk-position-bottom-left uk-card-small">
                                     <div class="uk-card-body">
                                         <div class="uk-transition-slide-left-small uk-visible@m">
