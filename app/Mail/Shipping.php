71a0a4dddc9f6e08b1cd2baa0335bc1566fdfca3
Samsul Anwar <sam.aan46@gmail.com>
2018-02-15 14:52:09 +0700

    fixed mail for queue

--- app/Mail/Shipping.php
+++ app/Mail/Shipping.php
@@ -6,20 +6,24 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Order;
+use App\Services\CurrencyService;
 
 class Shipping extends Mailable
 {
     use Queueable, SerializesModels;
     private $order;
+    private $lang;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($order)
+    public function __construct(Order $order, $lang)
     {
         $this->order = $order;
+        $this->lang = $lang;
     }
 
     /**
@@ -30,8 +34,34 @@ class Shipping extends Mailable
     public function build()
     {
         return $this->markdown('emails.shipping', [
-            'order' => $this->order
+            'order' => $this->shipping()
         ])
             ->subject(trans('app.shipping_subject'));
+    }
+
+    private function shipping()
+    {
+        $order = $this->order;
+        $currencyService = (new CurrencyService);
+
+        $currencyService->setLang($this->lang);
+
+        $exchange = $currencyService->getCurrentCurrency();
+        //inject currency
+        $order->order_total_idr = $order->order_subtotal + $order->shipping_cost;
+        $order->order_subtotal = $order->order_subtotal / $exchange->value;
+        $order->shipping_cost = $order->shipping_cost / $exchange->value;
+        $order->symbol = $exchange->symbol;
+
+        $order->details = $order->details->map(function ($entry) use ($exchange){
+            return [
+                'product_name' =>  $entry['product_name'],
+                'qty' =>  $entry['qty'],
+                'image' =>  $entry->productStock->product->images->first()->photo,
+                'price' =>  $entry['price']/ $exchange->value,
+            ];
+        });
+
+        return $order;
     }
 }
