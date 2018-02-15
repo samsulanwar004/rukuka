b4ddbfb3e0c696a2e73ca3591f83e5c619f5ae63
AoliaJack <joko.aolia98@gmail.com>
2018-02-15 13:22:02 +0700

    change mobile footer, change offcanvas, fix mobile search, change navbar mobile

--- resources/views/partials/mobile_nav.blade.php
+++ resources/views/partials/mobile_nav.blade.php
@@ -1,12 +1,12 @@
 <div class="uk-section uk-section-xsmall uk-padding-remove uk-margin-reove">
-  <div class="uk-section uk-section-default uk-section-xsmall uk-padding-remove-vertical">
+  {{-- <div class="uk-section uk-section-default uk-section-xsmall uk-padding-remove-vertical">
     <div class="uk-container uk-container-small">
       <div class="uk-panel uk-text-center">
         <a href="/" class="uk-link-reset"><img src="{{ imageCDN(config('common.logo')) }}" alt="rukuka" width="90"></a>
       </div>
     </div>
   </div>
-  <hr class="uk-margin-remove uk-padding-remove">
+  <hr class="uk-margin-remove uk-padding-remove"> --}}
   <div  uk-sticky="bottom: #hash; animation: uk-animation-slide-top;">
     <div class="uk-section uk-section-default uk-section-xsmall uk-padding-small uk-box-shadow-medium">
       <div class="uk-container uk-container-small">
@@ -28,13 +28,13 @@
                       locale="{{ json_encode(trans('app')) }}"
               ></navigation-mobile>
             </div>
-            <div class="uk-navbar-left uk-flex-1 test-overlay" hidden>
+            <div class="uk-navbar-left uk-flex-1 uk-margin-top test-overlay" hidden>
               <div class="uk-width-expand">
                 {{ Form::open(array('url' => '/search', 'method' =>'get','files' => true,'class' => 'uk-search uk-search-navbar uk-width-1-1')) }}
                 <input class="uk-search-input" name="keyword" type="search" required placeholder="find our product" autofocus>
                 {{ Form::close() }}
               </div>
-              <a class="uk-navbar-toggle" uk-close uk-toggle="target: .test-overlay; animation: uk-animation-fade" href="#"></a>
+              <a class="uk-navbar-toggle uk-padding-remove" uk-close uk-toggle="target: .test-overlay; animation: uk-animation-fade" href="#"></a>
             </div>
           </div>
         </div>
