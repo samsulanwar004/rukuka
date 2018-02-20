e63428b5115634f83a78e1cfd2db05a1d07eeefa
Samsul Anwar <sam.aan46@gmail.com>
2018-02-20 14:23:36 +0700

    fixed auto language by ip

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -15,7 +15,7 @@ class Language
 {
     public function handle($request, Closure $next)
     {
-Session::forget('applocale');
+
         if (Session::has('applocale') AND array_key_exists(Session::get('applocale'), Config::get('languages'))) {
             App::setLocale(Session::get('applocale'));
         }
