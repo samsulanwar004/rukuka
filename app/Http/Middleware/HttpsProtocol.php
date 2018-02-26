092b9668d9383ee755b29ac03d8451202c3ea6c1
Samsul Anwar <sam.aan46@gmail.com>
2018-02-26 13:41:34 +0700

    new middleware for auto to https

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if (!$request->secure() &&  in_array(env('APP_ENV'), array('developer', 'production')) ) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
