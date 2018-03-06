10a496fbcbc3b06ca7c1c8b689c623b3b66190cf
Samsul Anwar <sam.aan46@gmail.com>
2018-03-06 14:31:16 +0700

    fixed after login

--- app/Http/Controllers/Frontend/LoginController.php
+++ app/Http/Controllers/Frontend/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends BaseController
     public function showLoginPage()
     {
         $ref = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '';
-        $ref = rtrim($ref, '/');
+        $ref = $ref === null ? rtrim($ref, '/') : $this->redirectAfterLogin;
 
         if (session()->has('as.checkout')) {
             $ref = url('checkout');
@@ -125,9 +125,7 @@ class LoginController extends BaseController
             return back()->withErrors($e->getMessage());
         }
 
-        if ($request->input('return_url') == url('/')) {
-            return redirect($this->redirectAfterLogin)->with('success', 'Login successfully!');
-        } elseif ($request->input('return_url') == url('logout')) {
+        if ($request->input('return_url') == url('/') || $request->input('return_url') == url('logout') || $request->input('return_url') == url('login')) {
             return redirect($this->redirectAfterLogin)->with('success', 'Login successfully!');
         } elseif ($request->input('return_url') == url('tracking/order/result')) {
             return redirect($this->redirectToHistory)->with('success', 'Login successfully!');
@@ -166,9 +164,7 @@ class LoginController extends BaseController
         $url = session()->get('as.checkout.social');
         session()->forget('as.checkout.social');
         
-        if ($url == url('/')) {
-            return redirect($this->redirectAfterLogin)->with('success', 'Login successfully!');
-        } elseif ($url == url('logout')) {
+        if ($url == url('/') || $url == url('logout') || $url == url('login')) {
             return redirect($this->redirectAfterLogin)->with('success', 'Login successfully!');
         } else {
             return redirect($url)->with('success', 'Login successfully!');
