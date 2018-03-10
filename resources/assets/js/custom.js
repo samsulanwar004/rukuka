f5a80bc1c45bf9ab662092bfe849d7eeddb718ad
AoliaJack <joko.aolia98@gmail.com>
2018-03-10 15:32:15 +0700

    ready table for gulp production

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -4,12 +4,11 @@ function getTokenValue() {
 
 $(window).on('load', function() { // makes sure the whole site is loaded
 	$('#status').fadeOut(); // will first fade out the loading animation
-	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-	$('body').delay(350).css({'overflow':'visible'});
+	$('#preloader').delay(0).fadeOut('fast'); // will fade out the white DIV that covers the website.
+	$('body').delay(0).css({'overflow':'visible'});
 })
 
 $('#add-image').on('click', function (e) {
   e.preventDefault();
   $('div#form-body-image').append("<hr><div class='form-group'><label>Name Photo</label><input type='text' name='name[]' required class='form-control'/><label>Image</label><input type='file' name='image[]' required class='form-control'/></div>");
 });
-
