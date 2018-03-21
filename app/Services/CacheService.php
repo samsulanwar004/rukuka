5b5314ba37643dd861123f032d8f699d634c2a11
Samsul Anwar <sam.aan46@gmail.com>
2018-03-21 11:27:19 +0700

    add service for cache

---
+++ app/Services/CacheService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Cache;
+
+class CacheService
+{
+	const SETTING_HOME_CACHE = 'setting.home.cache';
+    const SETTING_MEN_CACHE = 'setting.men.cache';
+    const SETTING_WOMEN_CACHE = 'setting.women.cache';
+    const SLIDER_HOME_CACHE = 'slider.home.cache';
+    const SLIDER_MEN_CACHE = 'slider.men.cache';
+    const SLIDER_WOMEN_CACHE = 'slider.women.cache';
+    const DESIGNER_CACHE = 'designer.cache';
+    CONST MENU_CACHE = 'menu.cache';
+
+	public function clearCacheSetting()
+	{
+		Cache::forget(self::SETTING_HOME_CACHE);
+		Cache::forget(self::SETTING_MEN_CACHE);
+		Cache::forget(self::SETTING_WOMEN_CACHE);
+	}
+
+	public function clearCacheSlider()
+	{
+		Cache::forget(self::SLIDER_HOME_CACHE);
+		Cache::forget(self::SLIDER_WOMEN_CACHE);
+		Cache::forget(self::SLIDER_MEN_CACHE);
+	}
+
+	public function clearCacheDesigner()
+	{
+		Cache::forget(self::DESIGNER_CACHE);
+	}
+
+	public function clearCacheMenu()
+	{
+		Cache::forget(self::MENU_CACHE);
+	}
+}
