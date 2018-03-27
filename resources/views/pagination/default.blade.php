00dd542f895758e2c0d277236e62f6ff53fe2d5b
AoliaJack <joko.aolia98@gmail.com>
2018-03-27 15:24:14 +0700

    push change shop

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -1,13 +1,13 @@
 @if ($paginator->lastPage() > 1)
     {{ trans('app.page') }} : {{ $paginator->currentPage() }} {{ trans('app.of') }}  {{ $paginator->lastPage() }}  &nbsp;
-    <a href="{{ $paginator->url(1) }}" class="uk-icon-link {{ ($paginator->currentPage() <= 1) ? ' uk-disabled' : '' }}" 
-    uk-icon="icon: chevron-left"></a>
-    <a href="{{ $paginator->url($paginator->currentPage()+1) }}" class="uk-icon-link {{ ($paginator->currentPage() >= $paginator->lastPage()) ? ' uk-disabled' : '' }}" 
-    uk-icon="icon: chevron-right" ></a>
+    <a href="{{ $paginator->url(1) }}" class="uk-icon-link {{ ($paginator->currentPage() <= 1) ? ' uk-disabled' : '' }}"
+    ><i class="material-icons" style="font-size: 18px; vertical-align:middle">chevron_left</i></a>
+    <a href="{{ $paginator->url($paginator->currentPage()+1) }}" class="uk-icon-link {{ ($paginator->currentPage() >= $paginator->lastPage()) ? ' uk-disabled' : '' }}"
+    ><i class="material-icons" style="font-size: 18px; vertical-align:middle">chevron_right</i></a>
 @else
     {{ trans('app.page') }}  : 1 {{ trans('app.of') }}  1  &nbsp;
-    <a href="#" class="uk-icon-link uk-disabled" 
-    uk-icon="icon: chevron-left"></a>
-    <a href="#" class="uk-icon-link uk-disabled" 
-    uk-icon="icon: chevron-right" ></a>
+    <a href="#" class="uk-icon-link uk-disabled"
+    ><i class="material-icons" style="font-size: 18px; vertical-align:middle">chevron_left</i></a>
+    <a href="#" class="uk-icon-link uk-disabled"
+    ><i class="material-icons" style="font-size: 18px; vertical-align:middle">chevron_right</i></a>
 @endif
