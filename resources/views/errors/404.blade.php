0b5bad2f4f65a7c3763c68af7f2a67a2c37b713c
Samsul Anwar <sam.aan46@gmail.com>
2018-04-12 16:06:11 +0700

    when access link post redirect 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -33,12 +33,34 @@
        font-size: 100px;
        margin-bottom: 40px;
     }
+
+        /* Style buttons */
+    .btn {
+        background-color: #666666; /* Blue background */
+        border: none; /* Remove borders */
+        color: white; /* White text */
+        padding: 12px 16px; /* Some padding */
+        font-size: 16px; /* Set a font size */
+        cursor: pointer; /* Mouse pointer on hover */
+    }
+
+    /* Darker background on mouse-over */
+    .btn:hover {
+        background-color: #0e0e0e;
+    }
 </style>
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+<script type="text/javascript">
+  function goBack() {
+      window.history.back();
+  }
+</script>
 </head>
 <body>
     <div class="container">
         <div class="content">
             <div class="title">Opss...Page Not Found!</div>
+            <center><button class="btn" onclick="goBack()"><i class="fas fa-chevron-circle-left"></i></button></center>
         </div>
     </div>
 </body>
